88d4bc7249621fbb0b5f72798f7ff3a99903713c
wildanindi <wildanindi2@gmail.com>
2024-04-04 17:06:58 +0700

    menambah prosedur untuk mencari index yang lebih besar dari 0 dan kurang lebih atau sama dengan 20

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -9,8 +9,15 @@ void input()
 {
 	while (true)
 	{
-
+		cout << "Enter the number of elements in the array : ";
+		cin >> n;
+		if ((n > 0)) && (n <= 20))
+		break;
+		else
+			cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
 	}
+
+	
 }
 
 
