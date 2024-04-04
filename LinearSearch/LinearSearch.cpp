604381be67807a69be19e408d2ef42369f66c080
wildanindi <wildanindi2@gmail.com>
2024-04-04 17:12:01 +0700

    menambahkan fungsi for untuk melakukan perulangan pada array

--- LinearSearch/LinearSearch.cpp
+++ LinearSearch/LinearSearch.cpp
@@ -18,6 +18,14 @@ void input()
 	}
 
 	
+	cout << "\n------------------------\n";
+	cout << "Enter array elements\n";
+	cout << "------------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
 }
 
 
